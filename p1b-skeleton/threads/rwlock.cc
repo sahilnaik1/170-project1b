165ffa83fc8a60447fdd0d2aaaee2039ef7873f0
Jake Yim <jakeyim@umail.ucsb.edu>
2022-04-27 16:57:02 -0700

    project1a

--- p1b-skeleton/threads/rwlock.cc
+++ p1b-skeleton/threads/rwlock.cc
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <pthread.h>
 #include <iostream> //
 
 
@@ -8,11 +9,49 @@
 //Your solution for  Task 3 
 //TODO
 
-RWLock::RWLock(){ }
+RWLock::RWLock() { 
+    pthread_cond_init(&okToRead, 0);
+    pthread_cond_init(&okToWrite, 0);
+    pthread_mutex_init(&lock, 0);
+    AR, WR, AW, WW = 0,0,0,0;
+}
+RWLock::~RWLock() { 
 
-RWLock::~RWLock(){ }
-
-void RWLock::startRead(){ }
-void RWLock::doneRead(){ }
-void RWLock::startWrite(){ }
-void RWLock::doneWrite(){ }
+}
+void RWLock::startRead() { 
+    pthread_mutex_lock(&lock);
+    while ((AW + WW) > 0) {
+        WR++;
+        pthread_cond_wait(&okToRead, &lock);
+        WR--;
+    }
+    AR++;
+    pthread_mutex_unlock(&lock);
+}
+void RWLock::doneRead() { 
+    pthread_mutex_lock(&lock);
+    AR--;
+    if (AR == 0 && WW > 0)
+        pthread_cond_signal(&okToWrite);
+    pthread_mutex_unlock(&lock);
+}
+void RWLock::startWrite() { 
+    pthread_mutex_lock(&lock);
+    while( (AW + AR) > 0){
+        WW++;
+        pthread_cond_wait(&okToWrite, &lock);
+        WW--;
+    }
+    AW++;
+    pthread_mutex_unlock(&lock);
+}
+void RWLock::doneWrite() { 
+    pthread_mutex_lock(&lock);
+    AW--;
+    if (WW > 0) {
+        pthread_cond_signal(&okToWrite);
+    } else if (WR > 0) {
+        pthread_cond_broadcast(&okToRead);
+    }
+    pthread_mutex_unlock(&lock);
+}
--- p1b-skeleton/threads/rwlock.h
+++ p1b-skeleton/threads/rwlock.h
@@ -5,8 +5,12 @@
 
 class RWLock{
 private:
-      //Add  fields for  RWLock. Your solution to Task 3
-      //TODO
+    	//Add  fields for  RWLock. Your solution to Task 3
+    	//TODO
+
+		pthread_cond_t okToRead, okToWrite;
+		pthread_mutex_t lock;
+		int AR, WR, AW, WW;
 public:
     	RWLock();
     	~RWLock();
